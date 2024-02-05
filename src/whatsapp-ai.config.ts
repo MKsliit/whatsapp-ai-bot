60c3cdbd7860f6b571e0d8ad2af40d9d2f01f0a7
Hewage L G M K <148169760+MKsliit@users.noreply.github.com>
2024-02-05 19:54:15 +0530

    Update whatsapp-ai.config.ts

--- src/whatsapp-ai.config.ts
+++ src/whatsapp-ai.config.ts
@@ -5,30 +5,30 @@ const config: Config = {
     chatGPTModel: "gpt-3.5-turbo", // learn more about GPT models https://platform.openai.com/docs/models
     models: {
         ChatGPT: {
-            prefix: '!chatgpt', // Prefix for the ChatGPT model
+            prefix: '.gpt', // Prefix for the ChatGPT model
             enable: true // Whether the ChatGPT model is enabled or not
         },
         DALLE: {
-            prefix: '!dalle', // Prefix for the DALLE model
+            prefix: '.dalle', // Prefix for the DALLE model
             enable: true // Whether the DALLE model is enabled or not
         },
         StableDiffusion: {
-            prefix: '!stable', // Prefix for the StableDiffusion model
+            prefix: '.stable', // Prefix for the StableDiffusion model
             enable: true // Whether the StableDiffusion model is enabled or not
         },
         GeminiVision: {
-            prefix: '!gemini-vision', // Prefix for the GeminiVision model
+            prefix: '.gemini-vision', // Prefix for the GeminiVision model
             enable: true // Whether the GeminiVision model is enabled or not
         },
         Gemini: {
-            prefix: '!gemini', // Prefix for the Gemini model
+            prefix: '.gemini', // Prefix for the Gemini model
             enable: true // Whether the Gemini model is enabled or not
         },
         Custom: [
             {
                 /** Custom Model */
                 modelName: 'whatsapp-ai-bot', // Name of the custom model
-                prefix: '!bot', // Prefix for the custom model
+                prefix: '.bot', // Prefix for the custom model
                 enable: true, // Whether the custom model is enabled or not
                 /**
                     * context: "file-path (.txt, .text, .md)",
